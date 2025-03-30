65662d3ed7f99465347e5fa01fc0c7cc38ee0cf6
codrinursachi <codrinursachi@gmail.com>
2025-03-30 20:43:31 +0300

    add first_name and last_name columns in user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_30_172009) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_30_174253) do
+  create_table "folders", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_folders_on_user_id"
+  end
+
+  create_table "logins", force: :cascade do |t|
+    t.string "name"
+    t.string "login_name"
+    t.string "login_password"
+    t.text "notes"
+    t.boolean "is_favorite"
+    t.integer "folder_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["folder_id"], name: "index_logins_on_folder_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -20,13 +40,26 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_30_172009) do
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
+  create_table "urls", force: :cascade do |t|
+    t.string "uri"
+    t.integer "login_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["login_id"], name: "index_urls_on_login_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email_address", null: false
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "folders", "users"
+  add_foreign_key "logins", "folders"
   add_foreign_key "sessions", "users"
+  add_foreign_key "urls", "logins"
 end
