39594ae4c6e497906beb53c4184ed5ca920d0085
codrinursachi <codrinursachi@gmail.com>
2025-07-06 10:26:32 +0300

    update webauthn config

--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -1,5 +1,5 @@
 WebAuthn.configure do |config|
-  config.allowed_origins = "http://localhost:5173" # Replace with your domain
+  config.allowed_origins = ["http://localhost:5173", "http://localhost:3001"] # Replace with your domain
   config.rp_name = "Password Manager"
   config.rp_id = "localhost"
 end
